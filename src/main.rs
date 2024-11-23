94345a0b2f4886db4f2b0718e72d5f65d5e85832
nanaisisi <131863615+nanaisisi@users.noreply.github.com>
2024-11-23 19:23:15 +0900

    2/3

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,82 @@ impl Grid {
 
     fn generate(&mut self) -> Result<(), &'static str> {
         let mut rng = rand::thread_rng();
+        let mut random_number;
+        let mut regeneration_flag = false;
         for i in 0..9 {
             for j in 0..9 {
-                println!("{}行{}列目の数字を生成します", i + 1, j + 1);
-                let mut random_number = rng.gen_range(1..=9);
-                while self.data[i].contains(&random_number)
-                    || self.data.iter().any(|row| row[j] == random_number)
-                {
-                    random_number = rng.gen_range(1..=9);
+                if cfg!(debug_assertions) {
+                    println!("{}行{}列目の数字を生成します", i + 1, j + 1);
+                }
+                loop {
+                    random_number = rng.gen_range(1..10);
+                    if cfg!(debug_assertions) {
+                        println!("乱数: {}", random_number);
+                    }
+                    if regeneration_flag == true {
+                        regeneration_flag = false;
+                        if cfg!(debug_assertions) {
+                            println!("再生成します。");
+                            self.display();
+                        }
+                        continue;
+                    }
+                    for k in 0..9 {
+                        if self.data[i][k] == random_number {
+                            regeneration_flag = true;
+                            if cfg!(debug_assertions) {
+                                println!(
+                                    "{}行{}列目の数字{}はすでに存在します",
+                                    i + 1,
+                                    j + 1,
+                                    random_number
+                                );
+                                println!(
+                                    "{}行{}列目の{}が重複しているため再生成します",
+                                    i + 1,
+                                    k + 1,
+                                    random_number
+                                );
+                            }
+                        }
+                        if self.data[k][j] == random_number {
+                            regeneration_flag = true;
+                            if cfg!(debug_assertions) {
+                                println!(
+                                    "{}行{}列目の数字{}はすでに存在します",
+                                    i + 1,
+                                    j + 1,
+                                    random_number
+                                );
+                                println!(
+                                    "{}行{}列目の{}が重複しているため再生成します",
+                                    k + 1,
+                                    j + 1,
+                                    random_number
+                                );
+                            }
+                        }
+                    }
+                    if regeneration_flag == false {
+                        break;
+                    }
                 }
                 self.data[i][j] = random_number;
-                println!(
-                    "{}行{}列目の数字{}を生成しました",
-                    i + 1,
-                    j + 1,
-                    self.data[i][j]
-                );
+                if cfg!(debug_assertions) {
+                    println!(
+                        "{}行{}列目の数字{}を生成しました",
+                        i + 1,
+                        j + 1,
+                        self.data[i][j]
+                    );
+                }
             }
+            if cfg!(debug_assertions) {
+                println!("{:?}行目の数字生成完了", i + 1);
+            }
+        }
+        if cfg!(debug_assertions) {
+            println!("数字生成完了");
         }
         Ok(())
     }
